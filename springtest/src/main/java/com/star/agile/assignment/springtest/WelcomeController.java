5cf0d4e5134af4adf1be7106544a96754c485a94
kunalchinchole <kunal.chinchole11.gmail.com>
2023-04-15 12:47:59 +0530

    Spring Boot App

---
+++ springtest/src/main/java/com/star/agile/assignment/springtest/WelcomeController.java
@@ -0,0 +1,13 @@
+package com.star.agile.assignment.springtest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+	@GetMapping("/welcome")
+	public String welcome() {
+		return "Welcome to SpringBoot App Development StarAgile";
+	}
+}
